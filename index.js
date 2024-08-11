4aa92c295712649874c84b13a2aaba326ac9e75c
MimFSD <mimislam878@gmail.com>
2024-08-12 00:27:48 +0600

    recover import issue

--- index.js
+++ index.js
@@ -1,100 +1,39 @@
-const express = require("express");
-const app = express();
-const { MongoClient } = require("mongodb");
-const ObjectID = require("mongodb").ObjectID;
-const cors = require("cors");
-require("dotenv").config();
-
-const port = 5000;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zqlov.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const express = require('express')
+const cors = require('cors')
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
 
 
-  
-app.use(cors());
-app.use(express.json());
-
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-client.connect((err) => {
-  //Book collection
-  const booksCollection = client.db("ist-library").collection("books");  
-
-    // Book Issue collection
-    const issueBooksCollection = client
-    .db("ist-library")
-    .collection("issueBooks");
-  // librarian collection
-  const librarianCollection = client.db("ist-library").collection("librarian");
-
-   // books get
-   app.get("/getBooks", (req, res) => {
-    booksCollection.find().toArray((err, books) => {
-      res.send(books);
-    });
-  });
-
-   //issue book get
-   app.get("/getIssueBooks", (req, res) => {
-    issueBooksCollection.find().toArray((err, issueBooks) => {
-      res.send(issueBooks);
-    });
-  });
-  // books post
-  app.post("/addBook", (req, res) => {
-    const newBook = req.body;
-    console.log("new book", newBook);
-    booksCollection.insertOne(newBook).then((result) => {
-      console.log("Inserted Count ", result.acknowledged);
-      res.send(result.acknowledged > 0);
-    });
-  });
-   //issueBooks post
-   app.post("/issueBook", (req, res) => {
-    const issueBook = req.body;
-    console.log("Issue Book", issueBook);
-    issueBooksCollection.insertOne(issueBook).then((result) => {
-      console.log("Book Issue ", result.acknowledged);
-      res.send(result.acknowledged > 0);
-    });
-  });
-
-   // Department books
-   app.post("/booksByDepartment", (req, res) => {
-    // console.log(req);
-    const department = req.body.departmentName;
-    
-    booksCollection.find({ department }).toArray((err, books) => {
-      // console.log("books", books);
-      res.send(books);
-    });
-  });
+// middelware
+app.use(cors(
+  {
+    origin: [
+      'http://localhost:5173',
+      'https://library-management-7f05c.web.app',
+      'https://library-management-7f05c.firebaseapp.com'
+     ],
+    credentials: true
+  }
+))
+app.use(express.json())
+app.use(cookieParser())
 
 
+// verify token
+const verifyToken = async(req,res,next) =>{
+  const token = req.cookies?.token;
+  if(!token){
+    return res.status(401).send({message: "unauthorized access"})
+  }
+  jwt.verify(token, process.env.ACCES_TOKEN, (err,decode) => {
 
-    //update issue book
-    app.patch("/update/:id", (req, res) => {
-        const id = ObjectID(req.params.id);
-        issueBooksCollection
-          .updateOne(
-            { _id: id },
-            {
-              $set: {
-                bookName: req.body.bookName,
-                department: req.body.department,
-                issueDate: req.body.issueDate,
-                returnDate: req.body.returnDate,
-                roll: req.body.roll,
-                semester: req.body.semester,
-                studentName: req.body.studentName,
-              },
-            }
-          )
-          .then((result) => {
-            console.log("Update", result);
-            res.send(result.matchedCount > 0);
-          });
-      });
+    if(err){
+      return res.status(401).send({message: "unauthorized access"})
     }
+    req.user = decode;
+    next();
+  })
+}
