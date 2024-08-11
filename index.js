0147bd3fc20a0a2ee33c93f02c28f81b049e72a1
MimFSD <mimislam878@gmail.com>
2024-08-12 00:28:47 +0600

    borrowed books booking part done!

--- index.js
+++ index.js
@@ -37,3 +37,14 @@ const verifyToken = async(req,res,next) =>{
     next();
   })
 }
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntnzcww.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
