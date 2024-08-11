703b54abf4a2d83f03c198d809c9b6b4b6f053cb
MimFSD <mimislam878@gmail.com>
2024-08-12 00:16:38 +0600

    update details section are added

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 app.use(cors());
 app.use(express.json());
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
